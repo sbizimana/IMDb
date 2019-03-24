7331393ffb70b5e3fc006657036702cc9f6477ec
Stephen Bizimana <stephen.bizimana@gmail.com>
2019-03-23 22:47:22 -0400

    added functionality

--- temp.py
+++ temp.py
@@ -6,10 +6,10 @@ movies = imdb.IMDb()
 conn = sqlite3.connect("movies2.db")
 c = conn.cursor()
 
-c.execute('''DROP TABLE Performers''')
-c.execute('''DROP TABLE Films''')
-c.execute('''DROP TABLE Directors''')
-c.execute('''DROP TABLE MoviesData''')
+#c.execute('''DROP TABLE Performers''')
+#c.execute('''DROP TABLE Films''')
+#c.execute('''DROP TABLE Directors''')
+#c.execute('''DROP TABLE MoviesData''')
 c.execute('''CREATE TABLE IF NOT EXISTS Performers (CharacterID TEXT, Name TEXT)''')
 c.execute('''CREATE TABLE IF NOT EXISTS Films (MovieID TEXT, Title TEXT)''')
 c.execute('''CREATE TABLE IF NOT EXISTS Directors (DirectorID TEXT, Name TEXT)''')
@@ -19,10 +19,10 @@ c.execute('''CREATE TABLE IF NOT EXISTS InvalidIDs (MovieID TEXT)''')
 
 limit = 100
 try:
-    last_index = open('last_index1x.txt', 'r')
+    last_index = open('last_index1.txt', 'r')
     index = int(last_index.read()) + 1
 except FileNotFoundError as e:
-    index = 941
+    index = 1
 
 counter = 0
 while True:
@@ -59,17 +59,20 @@ while True:
                     c.execute('''INSERT INTO Directors VALUES (?,?)''', director)
 
             month_abbr = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6,
-                          'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12}
+                          'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12, None: None}
+
             has_date = False
             if 'original air date' in data:
                 has_date = True
-                date_and_month = True
-                if data['original air date'][0] in "0123":
+                if data['original air date'][0] in "0123" and data['original air date'][2] is ' ':
                     date_temp = int(data['original air date'][:2])
+                    month_temp = data['original air date'][3:6]
+                elif data['original air date'][3] is ' ':
+                    date_temp = None
+                    month_temp = data['original air date'][:3]
                 else:
                     date_temp = None
-                    date_and_month = False
-                month_temp = data['original air date'][3:6] if date_and_month else data['original air date'][0:3]
+                    month_temp = None
 
             genre = data['year'] if 'year' in data else None
             rating = data['rating'] if 'rating' in data else None
@@ -77,12 +80,13 @@ while True:
             date = date_temp if has_date else None
             month = month_abbr[month_temp] if has_date else None
             year = data['year'] if 'year' in data else None
+
             for x in range(len(data['cast'] if 'cast' in data else '1')):
                 for y in range(len(data['directors'])):
                     c.execute('''INSERT INTO MoviesData VALUES (?,?,?,?,?,?,?,?,?)''', [movie_id,
-                                                                                data['cast'][x].getID() if 'cast' in data else None,
-                                                                                data['directors'][y].getID(),
-                                                                                genre, rating, runtime, date, month, year])
+                                                                                        data['cast'][x].getID() if 'cast' in data else None,
+                                                                                        data['directors'][y].getID(),
+                                                                                        genre, rating, runtime, date, month, year])
 
             counter += 1
             print("Accepted index: %d \nCounter: %d\n" % (index, counter))
@@ -91,7 +95,7 @@ while True:
                     file.write(str(index))
                 break
     if not accepted:
-        print("Rejected index: %d \nCounter: %d" % (index, counter))
+        print("Rejected index: %d \nCounter: %d\n" % (index, counter))
     index += 1
 
 conn.commit()
--- temp1.py
+++ temp1.py
@@ -2,7 +2,7 @@ import imdb
 
 movies = imdb.IMDb()
 
-data = movies.get_movie('0000942').data
+data = movies.get_movie('0000501').data
 print(data['original air date'])
 
 performers = [[data['cast'][x].getID(), data['cast'][x].__str__()] for x in range(len(data['cast']))]
